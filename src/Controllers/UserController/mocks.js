5395f66fa8d567a66b373c81cbc9213eb1785a56
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2021-11-11 10:05:12 -0300

    add api and tests example

---
+++ src/Controllers/UserController/mocks.js
@@ -0,0 +1,33 @@
+const listDatabase = [
+  {
+    name: "Daniel Lopes",
+    age: 29,
+  },
+  {
+    name: "Pedro",
+    age: 10,
+  },
+];
+
+const listDatabaseWithFilter = [
+  {
+    name: "Daniel Lopes",
+    age: 29,
+  },
+];
+
+const userData = {
+  name: "Daniel Lopes",
+  age: 29,
+};
+
+const wrongUserData = {
+  age: 30,
+};
+
+module.exports = {
+  userData,
+  wrongUserData,
+  listDatabase,
+  listDatabaseWithFilter,
+};
